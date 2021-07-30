17f7a3a4b199471beb5b0093c8f4109440aa4124
Mateus Varela <mvarela038@gmail.com>
2021-07-30 00:25:13 -0300

    added events for url generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import findData from './services/urlRoute'
-import InputTest from './components/input'
-import Button from './components/button'
+import FormUrl from './components/form'
 import './App.css'
 
 function App() {
   return (
     <div>
-      <div className="header">Desafio</div>
       <div className="organize-inputs">
-        <InputTest /> <Button />
+        <FormUrl />
       </div>
     </div>
   )
--- src/services/urlRoute.js
+++ src/services/urlRoute.js
@@ -3,12 +3,11 @@ const API_URL = "https://usapim.herokuapp.com/short"
 
 const findData = async (url) => {
   const response = await axios.post(`${API_URL}`, url);
-  console.log(response, "resposta");
   return response
 }
 
 export default {
-  getHomeList: async (data) => {
-    return [{ items: await findData(data) }]
+  getUrlShort: async (data) => {
+    return { newUrl: await findData(data) }
   }
 };
